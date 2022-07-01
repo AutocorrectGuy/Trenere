893b5392ccdaf080c23cb56fb23fcdb04bd1d3e2
AutocorrectGuy <martins.sturainis.lspa@gmail.com>
2022-07-01 20:15:31 +0300

    test

--- client/src/components/views/landingPage/Section1.jsx
+++ client/src/components/views/landingPage/Section1.jsx
@@ -1,6 +1,6 @@
 export default function Section1() {
   return(
-    <div className={"flex max-w-7xl w-full mx-auto bg-black bg-opacity-50 min-h-[200px]"}>
+    <div className={"flex max-w-[1400px] w-full mx-auto bg-black bg-opacity-50 min-h-[200px]"}>
       {/* <div className="text-4xl mx-auto text-neutral-200">Esi sveiks, trener</div> */}
 
     </div>
