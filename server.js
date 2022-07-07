e5e47111b3c6e6d982f8ace01f68e56f55bc83da
AutocorrectGuy <martins.sturainis.lspa@gmail.com>
2022-07-07 10:41:21 +0300

    whitelisted cors array updated

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 const whitelist = [
   'http://localhost:3000',
   `http://localhost:${PORT}`,
-  'https://darrrbs.trenere.com'
+  'https://trenere.herokuapp.com'
 ];
 const corsConfig = {
   origin: function (origin, callback) {
